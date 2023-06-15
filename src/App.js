0e6227aa974681da448198255e8f67b95477e106
jobe-lumni <jobecamera@gmail.com>
2023-06-15 14:47:37 -0300

    refatorando a requisicao com useCallback e useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fecthMoviesHandler() {
+  const fecthMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -35,7 +35,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fecthMoviesHandler();
+  }, [fecthMoviesHandler]);
 
   let content = <p>Found no movies.</p>;
 
